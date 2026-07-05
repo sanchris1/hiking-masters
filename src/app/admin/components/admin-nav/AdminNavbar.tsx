df3cf46c8aeeaee83ccf933b6be63528f8785bc5
sanchris1 <samchris12737@gmail.com>
2026-07-05 23:26:35 +0300

    done with the page

--- src/app/admin/components/admin-nav/AdminNavbar.tsx
+++ src/app/admin/components/admin-nav/AdminNavbar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import InputComponent from "@/app/ui/Input";
 import { useState } from "react";
+import { BiBell, BiSearch } from "react-icons/bi";
+import { CiMail } from "react-icons/ci";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const AdminNavbar = () => {
   const [search, setSearch] = useState("");
@@ -14,20 +16,34 @@ const AdminNavbar = () => {
     <nav className="sticky  shadow w-full  px-4">
       <div className="flex items-center justify-between ">
         {/* search */}
-        <div className=" ">
-          <InputComponent
+        <div className=" relative">
+          <input
+            className="py-2 px-8 bg-surface-200 rounded-2xl w-100 outline-none border-2 text-primary font-semibold text-sm border-primary "
+            type="text"
             value={search}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setSearch(e.target.value)
-            }
-            placeholder="Search"
-            as="input"
-            name="search"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search for the expeditions"
           />
+          <BiSearch className="absolute top-1/2 left-1 -translate-y-1/2  text-primary size-6 pl-1" />
         </div>
+
         {/* dates and profile */}
         <div className=" flex items-center gap-8">
           <p className=" text-[16px] font-semibold text-text-accent">{today}</p>
+          <div className="relative  cursor-pointer  hover:bg-secondary/10 p-1 rounded-full ">
+            <BiBell className="size-5 text-primary" />
+            <span className="absolute top-0 right-0 bg-accent rounded-full size-2"></span>
+          </div>
+          <div className="relative cursor-pointer  hover:bg-secondary/10 p-1 rounded-full ">
+            <CiMail className="size-5 text-primary" />
+            <span className="absolute top-0 right-0 bg-accent rounded-full size-2"></span>
+          </div>
+
+          <span className="text-primary">|</span>
+
+          <span className="flex items-center gap-2 text-primary font-medium">
+            Profile <MdKeyboardArrowDown className="size-5" />
+          </span>
         </div>
       </div>
     </nav>
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 const AdminPages = () => {
-  return <div>AdminPages</div>;
+  const router = useRouter();
+
+  return (
+    <div className="flex items-center justify-center h-screen flex-col max-w-3xl gap-5 mx-auto">
+      <h3 className="font-semibold text-primary text-5xl text-center">
+        Welcome to the admin screen
+      </h3>
+      <p className="text-center text-secondary text-sm font-medium">
+        Stay on top of every expedition, booking, and customer interaction from
+        one central place. Everything you need to manage is right here.
+      </p>
+      <button
+        className="text-sm font-semibold text-surface-200 px-6 py-3 cursor-pointer bg-primary/95 hover:bg-primary transition-all duration-300 rounded-2xl"
+        onClick={() => router.push("/admin/dashboard")}
+      >
+        Go to the dashboard
+      </button>
+    </div>
+  );
 };
 
 export default AdminPages;
