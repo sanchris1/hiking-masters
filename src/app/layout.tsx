74f3ef64fff15fbb9309e9e7c6a8e3212ce3f864
sanchris1 <samchris12737@gmail.com>
2026-07-23 16:10:04 +0300

    added the analytics from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -35,7 +36,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col ">
         {children}
-
+        <Analytics />
         <Toaster />
       </body>
     </html>
